85f0161e4c10cd2264a2974a70a53d3294d1e3ea
pranav-sinha-workroom <pranav@leapsquarelabs.com>
2024-08-30 18:36:32 +0530

    empty row check

--- utils/excel_reader.py
+++ utils/excel_reader.py
@@ -9,9 +9,17 @@ def read_excel(file_path, sheet_name, column_mapping):
     
     for row in sheet.iter_rows(min_row=2, values_only=True):
         row_data = {}
+        is_empty_row = True
+        
         for excel_col, json_key in column_mapping.items():
             if excel_col in headers:
-                row_data[json_key] = row[headers[excel_col]]
-        data.append(row_data)
+                cell_value = row[headers[excel_col]]
+                if cell_value is not None and cell_value != "":
+                    is_empty_row = False
+                row_data[json_key] = cell_value
+        
+        if not is_empty_row:
+            data.append(row_data)
     
     return data
+
